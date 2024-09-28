6a0fe25f5d2a63a70a145f628a23fb115e27ec09
Denis Matveev <matveevdenis458@gmail.com>
2024-09-28 11:56:35 +0300

    feat(styles): added global.css, reset.css and connect to main.tsx feat(router): create routers

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import s from './header.module.css';
+
+const Header = () => {
+  return <header className={s.header}></header>;
+};
+
+export default Header;
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+import Layout from './Layout/Layout.tsx';
+
+export { Layout };
